4a6894751a9daf797e08edde56471741b7274822
Mike Gregory <mgregory8219@gmail.com>
2016-04-24 17:24:55 -0400

    added scrolling icons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424184146) do
+ActiveRecord::Schema.define(version: 20160424200559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160424184146) do
     t.string   "state"
     t.string   "zip"
     t.string   "meetup"
+    t.string   "logo"
   end
 
   create_table "users", force: :cascade do |t|
